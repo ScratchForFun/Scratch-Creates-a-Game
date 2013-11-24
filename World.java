148304684e4d833f7fb2119cf9c42c7cb653c578
ScratchForFun <magvaag@yahoo.no>
2013-11-24 21:44:16 +0100

    Update World.java

--- World.java
+++ World.java
@@ -28,33 +28,33 @@ public class World {
 	}
 	
 	private void generateFlat(Random random){
-		int y = 5;
+		int y = 0;
 		int y2 = y;
 		
 		for(int z = 0; z < world[0].length; z++){
 			for(int x = 0; x < world.length; x++){
-				world[x][z] = y2;
-				y2--;
-				
-				/*int numberLeft = x > 0 ? world[x-1][z] : 0;
+				/*world[z][x] = y2;
+				y2++;
+				*/
+				int numberLeft = x > 0 ? world[x-1][z] : 0;
 				int numberTop = z > 0 ? world[x][z-1] : 0;
 				
 				if(numberTop > numberLeft){
 					if(numberTop-numberLeft == 2) world[x][z] = numberTop-1;
-					else if(numberTop-numberLeft == 1) world[x][z] =/*random.nextInt(2) == 0 ? numberTop : numberLeft* numberTop;
+					else if(numberTop-numberLeft == 1) world[x][z] =/*random.nextInt(2) == 0 ? numberTop : numberLeft*/ numberTop;
 					else world[x][z] = random.nextInt(3) - 1 + numberTop;
 				}else if(numberTop < numberLeft){
 					if(numberLeft-numberTop == 2) world[x][z] = numberLeft-1;
-					else if(numberLeft-numberTop == 1) world[x][z] = /*random.nextInt(2) == 0 ? numberLeft : numberTop* numberLeft;
+					else if(numberLeft-numberTop == 1) world[x][z] = /*random.nextInt(2) == 0 ? numberLeft : numberTop*/ numberLeft;
 					else world[x][z] = random.nextInt(3) - 1 + numberTop;
 				}else{
 					world[x][z] = random.nextInt(3) - 1 + numberTop;
-				}*/
+				}
 
 				texture1[x][z] = Game.textures_world[0][random.nextInt(Game.textures_world[0].length)];
 				texture2[x][z] = Game.textures_world[0][random.nextInt(Game.textures_world[0].length)];
 			}
-			y2 = y;
+			y2 = y+(z-1);
 		}
 	}
 	
